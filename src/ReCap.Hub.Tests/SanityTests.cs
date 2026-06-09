41549d0a4811370ae691a064b59299d513ec40b0
JeanxPereira <jeanlima.vga@gmail.com>
2026-06-08 21:38:39 -0300

    test: add ReCap.Hub.Tests xUnit project

---
+++ src/ReCap.Hub.Tests/SanityTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace ReCap.Hub.Tests
+{
+    public class SanityTests
+    {
+        [Fact]
+        public void Harness_Runs()
+        {
+            Assert.True(true);
+        }
+    }
+}
